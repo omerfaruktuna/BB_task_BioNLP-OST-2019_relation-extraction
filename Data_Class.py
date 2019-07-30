7c5f4b5a154a8c67050640d2cfd8ecc56f96972d
omerfaruktuna <omerfaruktuna@gmail.com>
2019-07-30 16:40:43 +0300

    Update Data_Class.py

--- Data_Class.py
+++ Data_Class.py
@@ -14,19 +14,7 @@ class Data_Class ():
         self.result_helper = {}
         self.result_helper_list = []
         self.overlap_indices = []
+        self.deltas_helper = {}
+        self.deltas_helper_list = []
 
 
-"""
-class Data_Class ():
-
-    def __init__(self):
-        self.file_contents_by_sentence = []
-        self.file_names_ending_with_a1 = []
-        self.file_contents_a1 = []
-        self.updated_file_contents_a1 = []
-        self.file_contents_a1_with_sentenceID = []
-        self.sentences_indices = {}
-        self.sentences_indices_list = []
-        self.result_helper = {}
-        self.result_helper_list = []
-"""
